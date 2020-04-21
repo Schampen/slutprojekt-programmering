499138e7fb3c5c5e51fe0629c7e143c92db6a9ed
Schampen <hampus.mann@elev.ga.ntig.se>
2020-04-21 09:42:04 +0200

    börjat

---
+++ src/Head.java
@@ -0,0 +1,26 @@
+public class Head {
+    int x;
+    int y;
+    int lastX;
+    int lastY;
+
+    public void moveLeft() {
+        this.lastX = this.x;
+        this.x -= 1;
+    }
+
+    public void moveRight() {
+        this.lastX = this.x;
+        this.x += 1;
+    }
+
+    public void moveUp() {
+        this.lastY = this.y;
+        this.y -= 1;
+    }
+
+    public void moveDown() {
+        this.lastY = this.y;
+        this.y += 1;
+    }
+}
